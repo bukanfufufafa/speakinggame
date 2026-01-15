77a1278be8dc14050e6a0154e9e8cbd1d1bbd4bb
bukanfufufafa <nabilrizky249@gmail.com>
2026-01-16 01:36:43 +0700

    nambah

---
+++ Assets/script/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 12f;
+
+    [Header("Ground")]
+    public LayerMask groundLayer;
+
+    private Rigidbody2D rb;
+    [SerializeField] private bool isGrounded;
+    private float moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Input kiri kanan
+        moveInput = Input.GetAxis("Horizontal");
+
+        // Lompat
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Gerakan horizontal
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (((1 << other.gameObject.layer) & groundLayer) != 0)
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (((1 << other.gameObject.layer) & groundLayer) != 0)
+        {
+            isGrounded = false;
+        }
+    }
+}
